f278c7bdd38880f100e1b47a42b319f1081fd75c
Demian Hespe <hespe@kit.edu>
2021-02-01 15:45:11 +0100

    [mpi][bugfix] Erase removed elements

--- include/restore/mpi_context.hpp
+++ include/restore/mpi_context.hpp
@@ -22,7 +22,7 @@ typedef int original_rank_t;
 struct Message {
     std::shared_ptr<uint8_t> data;
     int                      size;
-    current_rank_t              rank;
+    current_rank_t           rank;
 };
 
 class RankManager {
@@ -54,8 +54,11 @@ class RankManager {
         std::vector<current_rank_t> currentRanks(originalRanks.size());
         MPI_Group_translate_ranks(
             _originalGroup, originalRanks.size(), originalRanks.data(), _currentGroup, currentRanks.data());
-        std::remove_if(
-            currentRanks.begin(), currentRanks.end(), [](const current_rank_t& rank) { return rank == MPI_UNDEFINED; });
+        currentRanks.erase(
+            std::remove_if(
+                currentRanks.begin(), currentRanks.end(),
+                [](const current_rank_t& rank) { return rank == MPI_UNDEFINED; }),
+            currentRanks.end());
         return currentRanks;
     }
 
