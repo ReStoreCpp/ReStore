8a0210e1e4fdba723d382a1f9ef8e0d0f4a5726f
Lukas Hübner <huebnerlukas@gmail.com>
2021-02-11 15:07:24 +0100

    [helpers] Add some useful user-defined literals

--- include/restore/helpers.hpp
+++ include/restore/helpers.hpp
@@ -1,9 +1,13 @@
 #ifndef RESTORE_HELPERS_H
 #define RESTORE_HELPERS_H
 
+#include <bits/stdint-uintn.h>
+
+// Suppress compiler warnings about unused variables
 #define UNUSED(expr) (void)(expr)
 
 #ifdef BACKWARD_CXX11
+// Use the backward-cpp library to print a pretty backtrace
 void print_stacktrace() {
     backward::StackTrace stacktrace;
     backward::Printer    printer;
@@ -12,4 +16,25 @@ void print_stacktrace() {
 }
 #endif
 
-#endif
+// Define some usefull user-defined literals
+inline constexpr uint8_t operator""_byte(unsigned long long arg) noexcept {
+    return static_cast<uint8_t>(arg);
+}
+
+inline constexpr uint8_t operator""_uint8(unsigned long long arg) noexcept {
+    return static_cast<uint8_t>(arg);
+}
+
+inline constexpr uint16_t operator""_uint16(unsigned long long arg) noexcept {
+    return static_cast<uint16_t>(arg);
+}
+
+inline constexpr uint32_t operator""_uint32(unsigned long long arg) noexcept {
+    return static_cast<uint32_t>(arg);
+}
+
+inline constexpr uint64_t operator""_uint64(unsigned long long arg) noexcept {
+    return static_cast<uint64_t>(arg);
+}
+
+#endif // Include guard
