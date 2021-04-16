5310f360845d4dd5bf98d6616c078d121399516f
Demian Hespe <hespe@kit.edu>
2021-04-16 15:26:21 +0200

    Implement graph reader and fix some bugs

--- benchmark/pageRank.cpp
+++ benchmark/pageRank.cpp
@@ -5,6 +5,7 @@
 #include <cmath>
 #include <cstddef>
 #include <cxxopts.hpp>
+#include <fstream>
 #include <iostream>
 #include <math.h>
 #include <mpi.h>
@@ -13,6 +14,7 @@
 #include <restore/helpers.hpp>
 #include <string.h>
 #include <string>
+#include <utility>
 
 using node_t = int;
 
@@ -21,13 +23,89 @@ using edge_id_t = int;
 struct edge_t {
     node_t from;
     node_t to;
+    edge_t(node_t u, node_t v) : from(u), to(v) {}
 };
 
 auto comm = MPI_COMM_WORLD;
 
 std::tuple<node_t, edge_id_t, std::vector<edge_t>, std::vector<node_t>> readGraph(std::string graph) {
-    UNUSED(graph);
-    return {};
+    int myRank, numRanks;
+    MPI_Comm_rank(comm, &myRank);
+    MPI_Comm_size(comm, &numRanks);
+
+    std::ifstream infile(graph);
+
+    std::string line;
+
+    node_t              numVertices           = 0;
+    edge_id_t           numEdges              = 0;
+    edge_id_t           numEdgesRead          = 0;
+    edge_id_t           numEdgesPerRank       = 0;
+    edge_id_t           numRanksWithMoreEdges = 0;
+    std::vector<edge_t> edges;
+    std::vector<node_t> outDegrees;
+    while (std::getline(infile, line)) {
+        if (line.empty()) {
+            continue;
+        }
+        if (line[0] == '#') {
+            continue;
+        }
+        std::istringstream iss(line);
+        char               letter;
+        node_t             firstNum, secondNum;
+        iss >> letter >> firstNum >> secondNum;
+        if (letter == 'p') {
+            if (numVertices != 0 || numEdges != 0) {
+                if (myRank == 0)
+                    std::cout << "Multiple lines starting with p. This is not supported!" << std::endl;
+                exit(1);
+            }
+            assert(numEdgesRead == 0);
+            numVertices = firstNum;
+            numEdges    = secondNum;
+            outDegrees.resize(static_cast<size_t>(numVertices));
+            numEdgesPerRank       = numEdges / numRanks;
+            numRanksWithMoreEdges = numEdges & numRanks;
+        } else if (letter == 'e') {
+            --firstNum;
+            --secondNum;
+            if (numVertices == 0 || numEdges == 0) {
+                if (myRank == 0)
+                    std::cout << "First edge before specifying number of vertices and edges. This is not supported!"
+                              << std::endl;
+                exit(1);
+            }
+            if (firstNum > numVertices) {
+                if (myRank == 0)
+                    std::cout << "Invalid vertex id " << firstNum << std::endl;
+                exit(1);
+            }
+            if (secondNum > numVertices) {
+                if (myRank == 0)
+                    std::cout << "Invalid vertex id " << secondNum << std::endl;
+                exit(1);
+            }
+            ++outDegrees[static_cast<size_t>(firstNum)];
+            const int lowerBound = numEdgesPerRank * myRank + std::min(myRank, numRanksWithMoreEdges);
+            const int upperBound = numEdgesPerRank * (myRank + 1) + std::min(myRank + 1, numRanksWithMoreEdges);
+            if (numEdgesRead >= lowerBound && numEdgesRead < upperBound) {
+                edges.emplace_back(firstNum, secondNum);
+            }
+            ++numEdgesRead;
+        } else {
+            if (myRank == 0)
+                std::cout << "Unsupported type: " << letter << std::endl;
+            exit(1);
+        }
+    }
+    if (numEdges != numEdgesRead) {
+        if (myRank == 0)
+            std::cout << "Expected " << numEdges << " edges but found " << numEdgesRead << std::endl;
+        exit(1);
+    }
+    assert(static_cast<edge_id_t>(edges.size()) == numEdgesPerRank + (myRank < numRanksWithMoreEdges));
+    return std::make_tuple(numVertices, numEdges, edges, outDegrees);
 }
 
 // double calcL2Norm(const std::vector<double>& vec) {
@@ -64,6 +142,8 @@ std::vector<double> pageRank(
     std::vector<double> tempPageRanks(static_cast<size_t>(numVertices), 0);
     const int           n = static_cast<int>(nodeDegrees.size());
     while (calcDiffL2Norm(prevPageRanks, currPageRanks, dampening) > tol) {
+        // if (myRank == 0)
+        //     std::cout << calcDiffL2Norm(prevPageRanks, currPageRanks, dampening) << std::endl;
         std::swap(prevPageRanks, currPageRanks);
         std::fill(currPageRanks.begin(), currPageRanks.end(), 0.0);
         for (const auto edge: edges) {
@@ -85,15 +165,18 @@ std::vector<double> pageRank(
 int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
 
+    int myRank;
+    MPI_Comm_rank(comm, &myRank);
+
     cxxopts::Options cliParser("pageRank", "Benchmarks a fault tolerant page rank algorithm.");
 
-    cliParser.add_options()                                                                          ///
-        ("graph", "path to an input graph file", cxxopts::value<std::string>())                      ///
-        ("d,dampening", "dampening factor.", cxxopts::value<unsigned long>()->default_value("0.85")) ///
+    cliParser.add_options()                                                                   ///
+        ("graph", "path to an input graph file", cxxopts::value<std::string>())               ///
+        ("d,dampening", "dampening factor.", cxxopts::value<double>()->default_value("0.85")) ///
         ("t,tolerance",
          "Tolerance for stopping PageRank iterations. Stops when the l2 norm of the difference between two iterations "
          "drops below the tolerance.",
-         cxxopts::value<unsigned long>()->default_value("0.000000001"))                                ///
+         cxxopts::value<double>()->default_value("0.000000001"))                                       ///
         ("r,repetitions", "Number of repitions to run", cxxopts::value<size_t>()->default_value("10")) ///
         ("h,help", "Print help message.");
 
@@ -104,19 +187,24 @@ int main(int argc, char** argv) {
     try {
         options = cliParser.parse(argc, argv);
     } catch (cxxopts::OptionException& e) {
-        std::cout << e.what() << std::endl << std::endl;
-        std::cout << cliParser.help() << std::endl;
+        if (myRank == 0)
+            std::cout << e.what() << std::endl << std::endl;
+        if (myRank == 0)
+            std::cout << cliParser.help() << std::endl;
         exit(1);
     }
 
     if (options.count("help")) {
-        std::cout << cliParser.help() << std::endl;
+        if (myRank == 0)
+            std::cout << cliParser.help() << std::endl;
         exit(0);
     }
 
     if (!options.count("graph")) {
-        std::cout << "Please provide a graph." << std::endl;
-        std::cout << cliParser.help() << std::endl;
+        if (myRank == 0)
+            std::cout << "Please provide a graph." << std::endl;
+        if (myRank == 0)
+            std::cout << cliParser.help() << std::endl;
         exit(1);
     }
 
