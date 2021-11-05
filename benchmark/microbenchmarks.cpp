d6003b73fb3ef6a75df0c97ff38968262dd53b30
Demian Hespe <hespe@kit.edu>
2021-11-05 14:38:42 +0100

    [benchmark] Add another benchmark for a more realistic recovery

--- benchmark/microbenchmarks.cpp
+++ benchmark/microbenchmarks.cpp
@@ -5,7 +5,9 @@
 #include <cstddef>
 #include <cstring>
 #include <filesystem>
+#include <limits>
 #include <mpi.h>
+#include <random>
 #include <restore/common.hpp>
 #include <utility>
 
@@ -106,7 +108,7 @@ BENCHMARK(BM_submitBlocks)          ///
         {MiB(1), MiB(16), MiB(32), MiB(64)}             //, MiB(128)} // bytes per rank
     });
 
-static void BM_pushBlocks(benchmark::State& state) {
+static void BM_pushBlocksRedistribute(benchmark::State& state) {
     // Each rank submits different data. The replication level is set to 3.
 
     // Parse arguments
@@ -165,12 +167,12 @@ static void BM_pushBlocks(benchmark::State& state) {
     }
     auto myStartBlock = static_cast<size_t>((myRankId() + 1) % numRanks()) * blocksPerRank;
 
+    std::vector<BlockType> recvData(blocksPerRank, BlockType(blockSize));
     // Measurement
     for (auto _: state) {
         UNUSED(_);
 
-        std::vector<BlockType> recvData(blocksPerRank, BlockType(blockSize));
-        auto                   start = std::chrono::high_resolution_clock::now();
+        auto start = std::chrono::high_resolution_clock::now();
         store.pushBlocksCurrentRankIds(
             blockRanges, [&recvData, myStartBlock](const std::byte* buffer, size_t size, ReStore::block_id_t blockId) {
                 assert(blockId >= myStartBlock);
@@ -194,15 +196,138 @@ static void BM_pushBlocks(benchmark::State& state) {
     }
 }
 
-BENCHMARK(BM_pushBlocks)            ///
+BENCHMARK(BM_pushBlocksRedistribute) ///
+    ->UseManualTime()                ///
+    ->Unit(benchmark::kMillisecond)  ///
+    ->ArgsProduct({
+        {8, 16, 32, 64, 128, 256, 512, KiB(1), MiB(1)}, // block sizes
+        {2, 3, 4},                                      // replication level
+        {MiB(1), MiB(16), MiB(32), MiB(64)}             //, MiB(128)} // bytes per rank
+    });
+const auto MAX_REPLICATION_LEVEL = 4;
+
+
+static void BM_pushBlocksSmallRange(benchmark::State& state) {
+    // Each rank submits different data. The replication level is set to 3.
+
+    // Parse arguments
+    auto blockSize        = throwing_cast<size_t>(state.range(0));
+    auto replicationLevel = throwing_cast<uint16_t>(state.range(1));
+    auto bytesPerRank     = throwing_cast<size_t>(state.range(2));
+    auto numRanksDataLoss = throwing_cast<size_t>(state.range(3));
+
+    assert(bytesPerRank % blockSize == 0);
+    auto blocksPerRank = bytesPerRank / blockSize;
+
+    auto recvBlocksPerRank = blocksPerRank * numRanksDataLoss / asserting_cast<size_t>(numRanks());
+
+    if (recvBlocksPerRank == 0) {
+        state.SkipWithError("Parameters set such that no one receives anything!");
+        return;
+    }
+
+    using ElementType = uint8_t;
+    using BlockType   = std::vector<ElementType>;
+
+    // Setup
+    uint64_t rankId    = asserting_cast<uint64_t>(myRankId());
+    size_t   numBlocks = static_cast<size_t>(numRanks()) * bytesPerRank / blockSize;
+
+    std::vector<BlockType> data;
+    for (uint64_t base: range(blocksPerRank * rankId, blocksPerRank * rankId + blocksPerRank)) {
+        data.emplace_back();
+        data.back().reserve(blockSize);
+        for (uint64_t increment: range(0ul, blockSize)) {
+            data.back().push_back(static_cast<ElementType>((base - increment) % std::numeric_limits<uint8_t>::max()));
+        }
+        assert(data.back().size() == blockSize);
+    }
+    assert(data.size() == blocksPerRank);
+
+    ReStore::ReStore<BlockType> store(
+        MPI_COMM_WORLD, replicationLevel, ReStore::OffsetMode::constant, sizeof(uint8_t) * blockSize);
+
+    unsigned counter = 0;
+
+    store.submitBlocks(
+        [](const BlockType& range, ReStore::SerializedBlockStoreStream& stream) {
+            stream.writeBytes(reinterpret_cast<const std::byte*>(range.data()), range.size() * sizeof(ElementType));
+        },
+        [&counter, &data]() -> std::optional<ReStore::NextBlock<BlockType>> {
+            auto ret = data.size() == counter
+                           ? std::nullopt
+                           : std::make_optional(ReStore::NextBlock<BlockType>(
+                               {counter + static_cast<size_t>(myRankId()) * data.size(), data[counter]}));
+            counter++; // We cannot put this in the above line, as we can't assume if the first argument of the pair
+                       // is bound before or after the increment.
+            return ret;
+        },
+        numBlocks);
+    assert(counter == data.size() + 1);
+
+    std::vector<std::pair<std::pair<ReStore::block_id_t, size_t>, int>> blockRanges;
+    blockRanges.reserve(asserting_cast<size_t>(numRanks()));
+    ReStore::block_id_t myStartBlock = std::numeric_limits<ReStore::block_id_t>::max();
+
+    // Use a random Range of numRanksDataLoss PEs whose data we redistribute
+    std::mt19937                                             rng(42);
+    std::uniform_int_distribution<std::mt19937::result_type> dist(
+        0, asserting_cast<unsigned long>(numRanks()) - numRanksDataLoss);
+
+    std::vector<BlockType> recvData(recvBlocksPerRank, BlockType(blockSize));
+    // Measurement
+    for (auto _: state) {
+        UNUSED(_);
+
+        assert(myStartBlock == std::numeric_limits<ReStore::block_id_t>::max());
+
+        blockRanges.clear();
+        ReStore::block_id_t startBlockId = dist(rng) * blocksPerRank;
+        for (int rank: range(numRanks())) {
+            blockRanges.emplace_back(std::make_pair(startBlockId, recvBlocksPerRank), rank);
+            if (rank == myRankId()) {
+                myStartBlock = startBlockId;
+            }
+            startBlockId += recvBlocksPerRank;
+            assert(startBlockId <= numBlocks);
+        }
+        assert(myStartBlock != std::numeric_limits<ReStore::block_id_t>::max());
+
+        auto start = std::chrono::high_resolution_clock::now();
+        store.pushBlocksCurrentRankIds(
+            blockRanges, [&recvData, myStartBlock](const std::byte* buffer, size_t size, ReStore::block_id_t blockId) {
+                assert(blockId >= myStartBlock);
+                auto index = blockId - myStartBlock;
+                assert(index < recvData.size());
+                // assert(recvData[index].size() == 0);
+                recvData[index].clear();
+                recvData[index].insert(
+                    recvData[index].end(), reinterpret_cast<const ElementType*>(buffer),
+                    reinterpret_cast<const ElementType*>(buffer + size));
+            });
+        benchmark::DoNotOptimize(recvData.data());
+        benchmark::ClobberMemory();
+        assert(std::all_of(recvData.begin(), recvData.end(), [blockSize](const BlockType& block) {
+            return block.size() == blockSize;
+        }));
+        auto end            = std::chrono::high_resolution_clock::now();
+        auto elapsedSeconds = std::chrono::duration_cast<std::chrono::duration<double>>(end - start).count();
+        MPI_Allreduce(MPI_IN_PLACE, &elapsedSeconds, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+        state.SetIterationTime(elapsedSeconds);
+        myStartBlock = std::numeric_limits<ReStore::block_id_t>::max();
+    }
+}
+
+BENCHMARK(BM_pushBlocksSmallRange)  ///
     ->UseManualTime()               ///
     ->Unit(benchmark::kMillisecond) ///
     ->ArgsProduct({
-        {8, KiB(1), MiB(1)},                // block sizes
-        {2, 3, 4},                          // replication level
-        {MiB(1), MiB(16), MiB(32), MiB(64)} //, MiB(128)} // bytes per rank
+        {8, 16, 32, 64, 128, 256, 512, KiB(1), MiB(1)}, // block sizes
+        {2, 3, 4},                                      // replication level
+        {MiB(1), MiB(16), MiB(32), MiB(64)},            //, MiB(128)} // bytes per rank
+        {1, 2, 4, 8},                                   // Number of ranks from which to get the data
     });
-const auto MAX_REPLICATION_LEVEL = 4;
+const auto MAX_DATA_LOSS_RANKS = 8;
 
 // This reporter does nothing.
 // We can use it to disable output from all but the root process
@@ -225,8 +350,9 @@ int main(int argc, char** argv) {
     assert(rank >= 0);
 
     // Do we have enough MPI ranks?
-    if (numRanks() < MAX_REPLICATION_LEVEL) {
-        std::cout << "Please call this benchmark with at least " << MAX_REPLICATION_LEVEL << " ranks." << std::endl;
+    if (numRanks() < std::max(MAX_REPLICATION_LEVEL, MAX_DATA_LOSS_RANKS)) {
+        std::cout << "Please call this benchmark with at least " << std::max(MAX_REPLICATION_LEVEL, MAX_DATA_LOSS_RANKS)
+                  << " ranks." << std::endl;
         return 1;
     }
 
