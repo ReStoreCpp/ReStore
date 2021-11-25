134c6501fe5e0bcdfff1bbc477193e82259d9687
Demian Hespe <hespe@kit.edu>
2021-11-25 10:14:26 +0100

    [bugfix][pushblocks] Don't hash uninitialized memory
    Was not the case before because of memory alignment in the std::pair<size_t, int>

--- include/restore/block_retrieval.hpp
+++ include/restore/block_retrieval.hpp
@@ -75,7 +75,7 @@ inline ReStoreMPI::original_rank_t getServingRank(
     }
 
     // Use same PE for all requests of a PE for a given BlockRange
-    auto blockRangeReceiverPair = std::make_pair(blockRange.id(), receivingRank);
+    auto blockRangeReceiverPair = std::make_pair(blockRange.id(), asserting_cast<size_t>(receivingRank));
     // TODO Think about seed
     auto servingIndex = XXH64(&blockRangeReceiverPair, sizeof(blockRangeReceiverPair), 42) % ranksWithBlockRange.size();
     return ranksWithBlockRange[servingIndex];
