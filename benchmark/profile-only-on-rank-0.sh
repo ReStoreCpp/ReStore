2705ce2541e4e1408639b6e483b363cb8d64e19b
Lukas Hübner <lukas.huebner@klhuebner.de>
2022-04-05 10:44:07 +0200

    Re-work and extend the profile-only-on-rank-0.sh script

--- benchmark/profile-only-on-rank-0.sh
+++ benchmark/profile-only-on-rank-0.sh
@@ -1,11 +1,40 @@
 #!/bin/bash
 
-PROFILING_RANK=0
-EXE_NAME="microbenchmarks"
+PROFILING_RANK="${PROFILING_RANK-0}"
+GPERF_LIB="/usr/lib/x86_64-linux-gnu/libprofiler.so.0"
+PPROF="${PPROF-$(which pprof)}"
+if [ "$PPROF" == "" ]; then
+    echo "pprof not found, please specify \$PPROF"
+    exit 2
+fi
+
+print-usage-and-exit() {
+    echo "Usage: $0 <tool> <binary> [<arg1>, ...]"
+    echo "where <tool> is one of: callgrind, gperf"
+    exit 1
+}
+
+# Parse arguments
+if [ $# -lt 1 ]; then
+    print-usage-and-exit
+fi
+
+PROFILING_TOOL="$1"
+if [ "$PROFILING_TOOL" != "callgrind" ] && [ "$PROFILING_TOOL" != "gperf" ]; then
+    print-usage-and-exit
+fi
+
+shift 1
+BINARY=$1
+CMD=("$@")
 
 if [ "$OMPI_COMM_WORLD_RANK" == "$PROFILING_RANK" ]; then
-    #LD_PRELOAD='/usr/lib/x86_64-linux-gnu/libprofiler.so.0' CPUPROFILE="$EXE_NAME.prof" "./$EXE_NAME"
-    valgrind --tool=cachegrind "./$EXE_NAME"
+    if [ "$PROFILING_TOOL" == "callgrind" ]; then
+        valgrind --tool=callgrind "${CMD[@]}"
+    elif [ "$PROFILING_TOOL" == "gperf" ]; then
+        LD_PRELOAD="$GPERF_LIB" CPUPROFILE="$BINARY.prof" "${CMD[@]}"
+        $PPROF --callgrind "$BINARY" "$BINARY.prof" > "$BINARY.callgrind"
+    fi
 else
-    "./$EXE_NAME"
+    "${CMD[@]}"
 fi
